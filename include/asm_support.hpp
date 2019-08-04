bfbb232fe8e1617c3ecee9c7e2e12279de48ddfa
revsic <revsic99@gmail.com>
2019-08-04 18:02:52 +0900

    asm support: Add register define

--- include/asm_support.hpp
+++ include/asm_support.hpp
@@ -12,4 +12,30 @@ namespace ASMSupport {
     std::tuple<BYTE*, size_t> SIBParser(BYTE* opc, PCONTEXT context);
 }
 
+// if x64 environment
+#ifdef _WIN64
+// declare register with rax-family
+#define RegisterAx Rax
+#define RegisterCx Rcx
+#define RegisterDx Rdx
+#define RegisterBx Rbx
+#define RegisterSp Rsp
+#define RegisterBp Rbp
+#define RegisterSi Rsi
+#define RegisterDi Rdi
+#define RegisterIp Rip
+// if x86 environment
+#else
+// declare register with eax-familty
+#define RegisterAx Eax
+#define RegisterCx Ecx
+#define RegisterDx Edx
+#define RegisterBx Ebx
+#define RegisterSp Esp
+#define RegisterBp Ebp
+#define RegisterSi Esi
+#define RegisterDi Edi
+#define RegisterIp Eip
+#endif
+
 #endif
