2f381bae361538353db037ae54f3000456a95f63
revsic <revsic99@gmail.com>
2019-08-05 04:48:39 +0900

    tracer: Passs bp controller to tracer

--- include/tracer.hpp
+++ include/tracer.hpp
@@ -3,14 +3,16 @@
 
 #include <Windows.h>
 
+#include "utils.hpp"
+
 // Interface for code trace handler.
 struct Tracer {
     // Default virtual destructor.
     virtual ~Tracer() = default;
     // Handle single step exception.
-    virtual void HandleSingleStep(PCONTEXT context) = 0;
+    virtual void HandleSingleStep(PCONTEXT context, Utils::SoftwareBP& bp) = 0;
     // Handle software breakpoint exception.
-    virtual void HandleBreakpoint(PCONTEXT context) = 0;
+    virtual void HandleBreakpoint(PCONTEXT context, Utils::SoftwareBP& bp) = 0;
 };
 
 #endif
