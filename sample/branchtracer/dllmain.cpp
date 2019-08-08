2ec5281a04b103f16a92e5de980308d35ca91611
revsic <revsic99@gmail.com>
2019-08-09 03:59:35 +0900

    dllmain: Alloc console

--- sample/branchtracer/dllmain.cpp
+++ sample/branchtracer/dllmain.cpp
@@ -4,9 +4,11 @@
 #include <debugger.hpp>
 
 Debugger CreateDebugger() {
+    // allocate console
+    AllocConsole();
     // create debugger
     Debugger dbg;
-    dbg.AddTracer(0, 0, std::make_unique<BranchTracer>("./log.txt"));
+    dbg.AddTracer(0, 0, std::make_unique<BranchTracer>("CONOUT$"));
     return std::move(dbg);
 }
 
