b85a59864c1b30437ad4aaae842c3db91068dd14
revsic <revsic99@gmail.com>
2019-08-10 03:59:13 +0900

    sample: Add readme_sample

---
+++ sample/readme_sample/dllmain.cpp
@@ -0,0 +1,34 @@
+#include <Windows.h>
+
+#include <vehdbi.hpp>
+
+struct EntrypointHandler : Handler {
+    void Handle(PCONTEXT context) override {
+        std::ofstream("CONOUT$") << "trigged on entrypoint" << std::endl;
+    }
+};
+
+VehDBI CreateDBI() {
+    AllocConsole();
+    // create debugger
+    VehDBI dbi;
+    // handler sample
+    size_t entrypoint = Utils::GetEntryPointAddress();
+    dbi.AddHandler(entrypoint, std::make_unique<EntrypointHandler>());
+    return std::move(dbi);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    static VehDBI dbi = CreateDBI();
+
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        VehDBI::Run(std::move(dbi));
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
--- script/build.ps1
+++ script/build.ps1
@@ -8,5 +8,6 @@ msbuild sample.sln /p:configuration=Release
 
 Copy-Item branchtracer\Release\branchtracer.dll ..\..\bin\branchtracer.dll
 Copy-Item dllinjector\Release\dllinjector.exe ..\..\bin\dllinjector.exe
+Copy-Item readme_sample\Release\readme_sample.dll ..\..\bin\readme_sample.dll
 
 Set-Location ..\..
